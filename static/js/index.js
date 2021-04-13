bc47925a7df27681627e35bd7785768d41c2d53c
airos6936 <81255746+airos6936@users.noreply.github.com>
2021-04-13 15:20:19 -0500

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -1,55 +1,36 @@
-//https://www.eclipse.org/paho/clients/js/
-
-function LED1_On() {
-    
-	console.log("Encendido");
-    alert("led on");
-    sendMessage("On");
-}
-function LED1_Off(){	
-	console.log("Apagado");
-    alert("led off");
-    sendMessage("Off");
-}
-
-
-
-
-
-
 // Create a client instance
   //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
   
   client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
-
+  var informacion="";
   // set callback handlers
   client.onConnectionLost = onConnectionLost;
   client.onMessageArrived = onMessageArrived;
   var options = {
-   useSSL: false,
+    useSSL: false,
     userName: "skillet776@yahoo.com",
     password: "1993orellana768",
     onSuccess:onConnect,
     onFailure:doFail
   }
-
+  
   // connect the client
   client.connect(options);
-   
+    
   // called when the client connects
   function onConnect() {
     // Once a connection has been made, make a subscription and send a message.
-    console.log("Conexion Establecida");
-	
-    client.subscribe("skillet776@yahoo.com/test");
-    sendMessage("hola desde mii web");
-	
+    console.log("Conectado...");
+  
+    client.subscribe("skillet776@yahoo.com/IoT");
+  
+  
   }
 
   function doFail(e){
     console.log(e);
 	
-  }
+  } 
 
   // called when the client loses its connection
   function onConnectionLost(responseObject) {
@@ -60,20 +41,25 @@ function LED1_Off(){
 
   // called when a message arrives
   function onMessageArrived(message) {
-    mensaje=message.payloadString.split("= ");
-    document.getElementById("sensor").innerHTML=mensaje[1];
-	document.getElementById("sensor1").innerHTML=mensaje[1];
-    console.log("MensajeRecibido:"+message.payloadString);
 
-  }
+    console.log("onMessageArrived:"+message.payloadString);
+    informacion=(message.payloadString).split(("?"));
     
-
-  function sendMessage(Texto){
-    message = new Paho.MQTT.Message(Texto);
-    message.destinationName = "skillet776@yahoo.com/test1";
-    client.send(message);
+    
   }
+  function fun1(){
+    var dat=document.getElementById("info");  
+    dat.innerHTML="";
+    dat.innerHTML=informacion[0];
+    }
+
+    
   
+  function fun2(){
+    var dat=document.getElementById("info");
+    dat.innerHTML="";
+    dat.innerHTML=informacion[1];
+  }
   
 
   
